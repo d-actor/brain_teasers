ad5f6d00b684e50cd3b0183b3075f8374092e911
Dan Actor <danltactor@gmail.com>
2017-11-26 15:33:45 -0700

    complete ruby prime checker

--- ruby/prime.rb
+++ ruby/prime.rb
@@ -7,6 +7,8 @@
 require "pry"
 require "colorize"
 
+@x = 2
+
 def menu
   print `clear`
   puts "---PRIME CHECKER---".yellow
@@ -23,13 +25,24 @@ def zero_check
     puts "Try again: ".yellow
     @number = STDIN.gets.strip.to_i
     zero_check
+  elsif @number == 1 or @number == 2
+    puts "Pretty basic prime there, friend!".green
   elsif @number > 0
     prime_check
   end
 end
 
 def prime_check
-
+  if @number % @x == 0
+    puts "Not a prime.".red
+  elsif @number % @x != 0
+    if @x < @number - 1
+      @x += 1
+      prime_check
+    else
+      puts "It is a prime indeed!".green
+    end
+  end
 end
 
 menu
