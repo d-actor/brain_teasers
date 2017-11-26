2dff57d56ec07b74732a727bd53953a081997df4
Dan Actor <danltactor@gmail.com>
2017-11-26 15:22:34 -0700

    add menu options

--- ruby/prime.rb
+++ ruby/prime.rb
@@ -1,8 +1,6 @@
 ## prime checker
 ## take in a number and check if it is prime
 
-# menu
-# get user input
 # prime check logic
 # output result
 
@@ -16,10 +14,22 @@ def menu
   puts "*Must be a whole, positive, non zero integer*"
   puts "Enter the number: ".yellow
   @number = STDIN.gets.strip.to_i
+  zero_check
+end
+
+def zero_check
+  if @number < 0 || @number == 0
+    puts "Requires a positive, whole, non-zero integer".red
+    puts "Try again: ".yellow
+    @number = STDIN.gets.strip.to_i
+    zero_check
+  elsif @number > 0
+    prime_check
+  end
 end
 
 def prime_check
-  
+
 end
 
 menu
